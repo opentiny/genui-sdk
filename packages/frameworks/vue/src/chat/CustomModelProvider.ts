064d8be671ff030d82e4463876c789be8c78a415
cenguanming <975402925@qq.com>
2026-01-24 09:33:17 +0800

    fix: support tool call streaming response

--- packages/frameworks/vue/src/chat/CustomModelProvider.ts
+++ packages/frameworks/vue/src/chat/CustomModelProvider.ts
@@ -83,6 +83,7 @@ export class CustomModelProvider extends BaseModelProvider {
     const decoder = new TextDecoder('utf-8');
     let buffer = '';
     const toolCallIdMap: Record<string, IMessageItem & { type: 'tool' }> = {};
+    let inProcessToolCallId;
     const { handleSchemaStream, clearSchemaState } = useSchemaStream();
     const chatMessage = reactive<IChatMessage>({
       role: 'assistant',
@@ -125,18 +126,32 @@ export class CustomModelProvider extends BaseModelProvider {
       toolCalls.forEach((toolCall) => {
         const {
           id,
-          function: { name, arguments: args },
+          function: { name, arguments: argsDelta },
         } = toolCall;
-        const toolCallItem: IMessageItem & { type: 'tool' } = reactive({
-          type: 'tool',
-          name: name,
-          formatPretty: true,
-          status: 'running',
-          content: args ? JSON.stringify({ arguments: args }, null, 2) : '',
-          id,
-        });
-        toolCallIdMap[id] = toolCallItem;
-        chatMessage.messages.push(toolCallItem);
+
+        let toolCallItem: IMessageItem & { type: 'tool' };
+        // 有id的就是首次工具调用返回
+        if (id) {
+          inProcessToolCallId = id;
+          toolCallItem = reactive({
+            type: 'tool',
+            name: name,
+            formatPretty: true,
+            status: 'running',
+            content: JSON.stringify({ arguments: argsDelta || '' }, null, 2),
+            id,
+          });
+          toolCallIdMap[id] = toolCallItem;
+          chatMessage.messages.push(toolCallItem);
+        } else {
+          toolCallItem = toolCallIdMap[inProcessToolCallId];
+          const prevArgs = JSON.parse(toolCallItem.content).arguments;
+          const nextArgs = prevArgs + (argsDelta || '');
+  
+          toolCallItem.content = JSON.stringify({ arguments: nextArgs }, null, 2);
+        }
+
+
 
         emitter.emit('notification', {
           type: 'tool',
