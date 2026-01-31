155da5d833b613f0979814f213a3db54f6a7f8ef
cenguanming <975402925@qq.com>
2026-01-31 10:48:34 +0800

    fix(playground): reduce condition logic

--- sites/playground/web/src/use-input-message.ts
+++ sites/playground/web/src/use-input-message.ts
@@ -4,18 +4,18 @@ import type { GenuiChat } from '@opentiny/genui-sdk-vue';
 export const useInputMessage = (chatInstance: Ref<ComponentPublicInstance<typeof GenuiChat>>) => {
   const initInputMessage = () => {
     const inputMessage = new URLSearchParams(window.location.search).get('input-message');
-    if (inputMessage) {
-      const conversation = chatInstance.value?.getConversation();
-      if (!conversation.state.loading) {
+    if (!inputMessage || !chatInstance.value) {
+      return;
+    };
+
+    const conversation = chatInstance.value?.getConversation();
+
+    const unwatch = watch(() => conversation.state.loading, (newValue) => {
+      if (!newValue) {
         chatInstance.value?.setInputMessage(inputMessage);
-      } else {
-        watch(() => conversation.state.loading, () => {
-          if (!conversation.state.loading) {
-            chatInstance.value?.setInputMessage(inputMessage);
-          }
-        }, { once: true });
+        unwatch();
       }
-    }
+    }, { immediate: true });
   };
   return {
     initInputMessage,
