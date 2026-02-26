79dc6f62eefe3a5471cb789270809bb5d4cdfa88
rhlin <r.h.lin@163.com>
2026-02-26 18:16:07 +0800

    test(genui-sdk-core): add testcase for stream-pattern-extractor

---
+++ packages/core/src/stream-pattern-extractor/__tests__/testcase/index.ts
@@ -0,0 +1,7 @@
+const prefix = 'testcase/';
+export const testCases = [ // TODO： 自动扫描
+  {
+    fileName: `${prefix}case-01/replay.txt`,
+    expectedResultFileName: `${prefix}case-01/out.txt`,
+  },
+];
---
+++ packages/core/src/stream-pattern-extractor/__tests__/utils/merge-stream.ts
@@ -0,0 +1,36 @@
+const never = new Promise(() => { });
+export function mergeStreams(streams: ReadableStream<string>[]): ReadableStream<{ value: string, type: number }> {
+  const readers = streams.map(s => s.getReader());
+  const reads: any = streams.map(() => null);
+  const dones: (Function)[] = [];
+  const allDone = Promise.all(streams.map(s => new Promise(resolve => {
+    dones.push(resolve);
+  })));
+
+  return new ReadableStream({
+    start: controller => {
+      allDone.then(() => {
+        controller.close();
+      });
+    },
+    pull: controller =>
+      Promise.race(
+        readers.map((r, i) =>
+          reads[i] ??= r.read().then(({ value, done }) => {
+            if (done) {
+              dones[i]();
+              return never;
+            }
+
+            controller.enqueue({ value, type: i });
+            reads[i] = null;
+          })
+        )
+      ),
+    cancel: reason => {
+      for (const reader of readers) {
+        reader.cancel(reason);
+      }
+    },
+  });
+};
