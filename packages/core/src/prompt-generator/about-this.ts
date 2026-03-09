cbb99196b0a161fc15d71ef1650dd0ea27cb04c5
cenguanming <975402925@qq.com>
2026-03-09 20:23:38 +0800

    fix: update prompt generator to markdown format

--- packages/core/src/prompt-generator/about-this.ts
+++ packages/core/src/prompt-generator/about-this.ts
@@ -1,48 +1,51 @@
 export const aboutThis = `
-## this上下文声明
-### this可访问范围：
+## this 上下文声明
 
-this在表达式JSExpression中可以访问，但不能在表达式内的行内函数访问；this也可以在JSFunction中访问。
+### this 可访问范围
 
-### this默认有以下几个属性：
+- \`this\` 在表达式 \`JSExpression\` 中可以访问，但不能在表达式内的行内函数中访问
+- \`this\` 也可以在 \`JSFunction\` 中访问
 
-- this.state：当前组件所有在state声明的变量;
-- 在methods里声明的方法可以直接用this属性访问，比如声明 hello方法，则通过this.hello()访问。
+### this 默认属性
 
+- \`this.state\`：当前组件所有在 \`state\` 中声明的变量
+- 在 \`methods\` 里声明的方法可以直接用 \`this\` 属性访问，例如声明 \`hello\` 方法，则通过 \`this.hello()\` 访问
 
-### this使用方法：
+### this 使用方法
 
 让函数访问到当前执行的上下文范围可以有以下几种方式：
 
-1.JSFunction模式
+**1. JSFunction 模式**
 
-如果正在使用JSFunction 比如 
-\`\`\`
+\`\`\`json
 {
     "type": "JSFunction",
     "value": "(event) => this.hello(event, name)"
 }
 \`\`\`
 
-2.JSExpression扩展参数模式
+**2. JSExpression 扩展参数模式**
 
-如果正在使用JSExpression，通过添加params参数，可以扩展参数，比如
-\`\`\`
+
+如果正在使用\`JSExpression\`，通过添加\`params\`参数，可以扩展参数，示例如下：
+\`\`\`json
 {
     "type": "JSExpression",
     "value": "this.hello",
     "params": ["name"]
 }
 \`\`\`
-注意：示例中，启用params的话，函数第一个参数是event 第二个参数才是name
-params参数必须是一个数组，数组中的每个元素都是字符串，每个字符串代表了当前的上下文scope变量名，不要传入'row.name'等表达式。
-JSExpression指向的函数第一个参数是event 第二个参数才是用户制定的params
 
-这两种方式都相当于执行了 this.hello(event, name)
+**启用 params 时需注意：**
+- 函数第一个参数是 \`event\`，第二个参数才是 \`name\`。
+- \`params\` 参数必须是一个字符串数组，每个字符串代表当前上下文 scope 变量名，不要传入 \`row.name\` 等表达式。
+- JSExpression 指向的函数第一个参数是 \`event\`，第二个参数才是用户指定的 \`params\`。
 
-### this不存在的属性
+这两种方式都相当于执行了 \`this.hello(event, name)\`。
 
-特别注意：this上下文并不存在this.setState方法，任何时候都不要使用， 直接给this.state赋值即可。
-重复三次：不存在this.setState方法，不存在this.setState方法，不存在this.setState方法。
+### this 不存在的属性
+
+- **特别注意：** \`this\` 上下文并不存在 \`this.setState\` 方法，任何时候都不要使用，直接给 \`this.state\` 赋值即可。  
+- **再次强调：** 不存在 \`this.setState\` 方法，不存在 \`this.setState\` 方法，不存在 \`this.setState\` 方法。
 `;
 
--- packages/core/src/prompt-generator/action.ts
+++ packages/core/src/prompt-generator/action.ts
@@ -1,12 +1,19 @@
 export const genCustomActionsPrompt = (actionDefinitions: any[]) => actionDefinitions?.length > 0 ? `
-以下是一些Action的定义：
-${JSON.stringify(actionDefinitions, null, 2)}
+## Action 定义
 
-如果需要使用Action可以直接写着Action的名称，不需要具体实现。
-Action的参数需要根据Action的定义来填写.
-发起Action调用可以在 JSFunction 里通过 \`this.callAction(actionName, params)\`来调用。
-例如：
+以下是一些 Action 的定义：
+
+\`\`\`json
+${JSON.stringify(actionDefinitions, null, 2)}
 \`\`\`
+
+- 如果需要使用 \`Action\`，直接写 \`Action\` 的名称即可，不需要具体实现
+- \`Action\` 的参数需要根据 \`Action\` 的定义来填写
+- 发起 \`Action\` 调用可以在 \`JSFunction\` 里通过 \`this.callAction(actionName, params)\` 来调用
+
+**示例：**
+
+\`\`\`json
 {
   "componentName": "span",
   "props": {
@@ -15,7 +22,7 @@ Action的参数需要根据Action的定义来填写.
       "value": "function() { this.callAction('continueChat', { message: '继续对话' }); }"
     }
   },
-  children: ["点击继续对话"]
+  "children": ["点击继续对话"]
 }
 \`\`\`
 ` : '';
