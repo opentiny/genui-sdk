4edd15c4e30bc11ecbac6592e7ac80e972c1ac17
cenguanming <975402925@qq.com>
2026-03-18 10:19:01 +0800

    fix: extract getPartialStartRegString function into common module and update imports

---
+++ packages/core/src/stream-pattern-extractor/common.ts
@@ -0,0 +1,5 @@
+export function getPartialStartRegString(flag: string) {
+    return flag.split('').reverse().reduce((acc, cur) => {
+      return `${cur}(${acc})?`;
+    }, '');
+  }
--- packages/core/src/stream-pattern-extractor/index.ts
+++ packages/core/src/stream-pattern-extractor/index.ts
@@ -2,3 +2,4 @@ export * from './pattern-extractor';
 export * from './schema-json-pattern';
 export * from './stream-pattern-extractor';
 export * from './think-tag-wrap-pattern';
+export * from './common';
