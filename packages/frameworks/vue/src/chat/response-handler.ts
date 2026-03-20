8bc606a9c6ef0bed547fd1fb0c4d328fcfb0b5b7
rhlin <r.h.lin@163.com>
2026-03-20 18:14:16 +0800

    fix(genui-sdk-vue/chat): fix response handler reasoning end

--- packages/frameworks/vue/src/chat/response-handler.ts
+++ packages/frameworks/vue/src/chat/response-handler.ts
@@ -175,6 +175,9 @@ export const defaultResponseHandlers: IResponseHandler<IStreamDelta>[] = [
       onReasoningEnd(context.chatMessage);
       return false;
     },
+    end: (context: any) => {
+      onReasoningEnd(context.chatMessage);
+    },
   },
   {
     name: 'toolCall',
