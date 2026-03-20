e7266911072d32d1e118fc645c84f11e587a1088
cenguanming <975402925@qq.com>
2026-03-20 14:35:49 +0800

    refactor: improve formatting of getPartialStartRegString function in common.ts

--- packages/core/src/stream-pattern-extractor/common.ts
+++ packages/core/src/stream-pattern-extractor/common.ts
@@ -1,6 +1,8 @@
 export function getPartialStartRegString(flag: string) {
-    return flag.split('').reverse().reduce((acc, cur) => {
+  return flag
+    .split('')
+    .reverse()
+    .reduce((acc, cur) => {
       return `${cur}(${acc})?`;
     }, '');
-  }
-  
+}
