d97880f82787eba813774d2f07f1ba3d19d0f50f
yy <yaoyun_bigger@outlook.com>
2026-03-27 14:38:47 +0800

    chore(benchmarks): remove index.ts file and its exports to clean up the benchmarks package

--- packages/benchmarks/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './framework';
-export * from './benchmark.config';
-export * from './samples';
-export * from './utils';
-export * from './generate-samples';
-export * from './run-report';
