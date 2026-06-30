5c92a98c6778773ea461280f8771b9cac86d15a9
cenguanming <975402925@qq.com>
2026-06-30 10:28:52 +0800

    refactor: remove unused built-in components (Box, Button, Img, Input, Slot) from the tiny-schema-renderer

--- projects/tiny-schema-renderer-react/src/builtin/Slot.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from 'react';
-
-export function Slot({ children }: { children?: ReactNode }) {
-  return <>{children}</>;
-}
--- projects/tiny-schema-renderer-react/src/builtin/index.ts
+++ projects/tiny-schema-renderer-react/src/builtin/index.ts
@@ -1,8 +1,3 @@
 export * from './html-tags';
-export * from './Box';
 export * from './Text';
-export * from './Img';
-export * from './Button';
-export * from './Input';
-export * from './Slot';
 export { builtinMaterials } from './builtin-materials';
