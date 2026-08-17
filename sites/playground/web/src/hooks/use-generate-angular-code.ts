5bb93d4fd9399db414051a4690d87469b912e2a4
itheima <3616127813@qq.com>
2026-08-17 11:18:47 +0800

    fix: resolve angular code-generator module in vue-tsc build

--- sites/playground/web/src/hooks/use-generate-angular-code.ts
+++ sites/playground/web/src/hooks/use-generate-angular-code.ts
@@ -1,4 +1,4 @@
-import { generateCode as generateAngularCode } from '@opentiny/genui-sdk-angular';
+import { generateCode as generateAngularCode } from '@opentiny/genui-sdk-angular/code-generator';
 
 const downloadTextFile = (filename: string, text: string): void => {
   const blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
