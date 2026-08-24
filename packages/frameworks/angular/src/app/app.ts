95263a86531d006f530876fd59677682aee6e405
rhlin <r.h.lin@163.com>
2026-08-24 20:55:30 +0800

    fix(gen-sdk-angular): clear demo

--- packages/frameworks/angular/src/app/app.ts
+++ packages/frameworks/angular/src/app/app.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 import { fetchSchemaStream } from '../fetch-schema-stream';
 import { FormsModule } from '@angular/forms';
 import { GenuiConfigProvider, GenuiRenderer } from '@opentiny/genui-sdk-angular';
-import { materials } from '@opentiny/genui-sdk-materials-angular-ng-devui/materials';
-import '@opentiny/genui-sdk-materials-angular-ng-devui/patch';
-import formJson from './form.json';
-import table1Json from './table.json';
-import table2Json from './table2.json';
-import table3Json from './table3.json';
-import table31Json from './table3.1.json';
+import { materials } from '@opentiny/genui-sdk-materials-angular-opentiny-ng/materials';
 
 @Component({
   selector: 'app-root',
@@ -18,21 +12,11 @@ import table31Json from './table3.1.json';
 })
 export class App {
   inputText = '';
-  schema: any = JSON.stringify(formJson);
-  table1Schema: any = JSON.stringify(table1Json);
-  table2Schema: any = JSON.stringify(table2Json);
-  table3Schema: any = JSON.stringify(table3Json);
-  table31Schema: any = JSON.stringify(table31Json);
-
+  schema: any = '';
   rendererKey = '';
   generating = false;
   protected readonly activeMaterials = materials;
 
-  ngOnInit() {
-    // import('./form.json').then(res => {
-    //   this.schema = res.default;
-    // });
-  }
   async handleSend() {
     if (!this.inputText.trim() || this.generating) return;
 
