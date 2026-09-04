aadfd2755772d02d5ea29dc797680f5ea53f301b
cenguanming <975402925@qq.com>
2026-09-04 17:39:00 +0800

    fix: remove useless builtin component

--- projects/tiny-schema-renderer-react/src/builtin/builtin-materials.tsx
+++ projects/tiny-schema-renderer-react/src/builtin/builtin-materials.tsx
@@ -1,12 +1,6 @@
-import type { ReactNode } from 'react';
 import { Text } from './Text';
 import type { ComponentRegistry } from '../materials';
 
-function Page({ children }: { children?: ReactNode }) {
-  return <div className="genui-page">{children}</div>;
-}
-
 export const builtinMaterials: ComponentRegistry = {
-  Page,
   Text,
 };
